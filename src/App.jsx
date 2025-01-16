cbd703bc9db501b6ff5dc43975c994eeae7f0b05
management_student <haiquanphung1807@gmail.com>
2025-01-16 10:14:37 +0700

    [#2]:addSection4]

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Section_4 from "./components/main/Section_4";
 import Section_3 from "./components/main/Section_3";
 import Section_2 from "./components/main/Section_2";
 import Section_1 from "./components/main/section_1";
@@ -12,6 +13,7 @@ function App() {
       <Section_1></Section_1>
       <Section_2></Section_2>
       <Section_3></Section_3>
+      <Section_4></Section_4>
     </>
   );
 }
